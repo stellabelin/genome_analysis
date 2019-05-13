a3c84fb1fbb41b2110d28e4195fbd72de8bacf62
Stella <stella.belin@me.com>
2019-05-13 15:28:06 +0200

    Maker step 2

--- scripts/6_genome_annotation/maker_step2_sbatch.sh
+++ scripts/6_genome_annotation/maker_step2_sbatch.sh
@@ -11,6 +11,9 @@
 module load bioinfo-tools
 module load maker
 module load snap
+module load GeneMark
+
+cp -v /sw/apps/bioinfo/GeneMark/4.33-es/rackham/gm_key /home/stellab/private/genome_analysis/scripts/6_genome_annotation/.gm_key
 
 cd /home/stellab/private/genome_analysis/results/6_genome_annotation/unzipped.maker.output/
 
