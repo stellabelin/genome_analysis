a44fa677ecf07de565b2ab2df36f1b309f595405
Stella <stella.belin@me.com>
2019-05-27 20:47:01 +0200

    Added quast

---
+++ scripts/5_assembly_quality/quast_all_sbatch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 02:00:00
+#SBATCH -J genome_assembly_evaluation_quast
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user stella.belin@me.com
+
+# Load modules
+module load bioinfo-tools
+module load quast
+
+quast.py /home/stellab/private/genome_analysis/results/5_assembly_quality/quast/all/spades.fasta \
+/home/stellab/private/genome_analysis/results/5_assembly_quality/quast/all/soapdenovo_27.fasta \
+/home/stellab/private/genome_analysis/results/5_assembly_quality/quast/all/soapdenovo_49.fasta \
+-o /home/stellab/private/genome_analysis/results/5_assembly_quality/quast/comparison \
+-R /home/stellab/private/genome_analysis/raw_data/unzipped.fasta
