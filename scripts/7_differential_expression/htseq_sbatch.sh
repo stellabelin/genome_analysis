f9706e4538ab0978252e6f4afd5756c3a68f41c3
Stella <stella.belin@me.com>
2019-05-27 16:18:17 +0200

    Htseq script

--- scripts/7_differential_expression/htseq_sbatch.sh
+++ scripts/7_differential_expression/htseq_sbatch.sh
@@ -11,7 +11,7 @@
 module load bioinfo-tools
 module load htseq
 
-head -27027 /home/stellab/private/genome_analysis/results/6_genome_annotation/unzipped.maker.output/unzipped.all.gff > \
+head -27026 /home/stellab/private/genome_analysis/results/6_genome_annotation/unzipped.maker.output/unzipped.all.gff > \
 /home/stellab/private/genome_analysis/results/6_genome_annotation/unzipped.maker.output/unzipped_no_fasta.all.gff
 
 htseq-count -f bam -s no -t gene -i ID /home/stellab/private/genome_analysis/results/7_differential_expression/accepted_hits.bam \
