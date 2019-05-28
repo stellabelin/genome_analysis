48f43c1d2a0cccf342b8012f4328aedeead5be1e
Stella <stella.belin@me.com>
2019-05-28 18:42:22 +0200

    Updated DESeq2

--- scripts/7_differential_expression/deseq_differential_expression.R
+++ scripts/7_differential_expression/deseq_differential_expression.R
@@ -16,9 +16,25 @@
 # library(RColorBrewer)
 # library(pheatmap)
 
+total_count <- read.table("/home/stellab/private/genome_analysis/results/7_differential_expression/htseq_count.txt")
+
 cs15_forelimb <- read.table("/home/stellab/private/genome_analysis/results/7_differential_expression/htseq_count.txt")
 cs15_hindlimb <- read.table("/home/stellab/private/genome_analysis/results/7_differential_expression/htseq_count.txt")
 cs16_forelimb <- read.table("/home/stellab/private/genome_analysis/results/7_differential_expression/htseq_count.txt")
 cs16_hindlimb <- read.table("/home/stellab/private/genome_analysis/results/7_differential_expression/htseq_count.txt")
 
-print(cs15_forelimb)
+names(cs15_forelimb)[2] <- "cs15_forelimb"
+names(cs15_hindlimb)[2] <- "cs15_hindlimb"
+names(cs16_forelimb)[2] <- "cs16_forelimb"
+names(cs16_hindlimb)[2] <- "cs16_hindlimb"
+
+cs15_forelimb <- head(cs15_forelimb,-5)
+cs15_hindlimb <- head(cs15_hindlimb,-5)
+cs16_forelimb <- head(cs16_forelimb,-5)
+cs16_hindlimb <- head(cs16_hindlimb,-5)
+
+both_dev_stages <- merge(cs15_forelimb,cs15_hindlimb,by="V1")
+both_dev_stages <- merge(both_dev_stages,cs16_forelimb,by="V1")
+both_dev_stages <- merge(both_dev_stages,cs16_hindlimb,by="V1")
+
+print(both_dev_stages)
