550a9dd71ee394c3935e9b693e2855dc3cd64e74
Stella <stella.belin@me.com>
2019-05-31 15:40:46 +0200

    RNA trimming

--- scripts/2_trimming/rna_trim.sh
+++ scripts/2_trimming/rna_trim.sh
@@ -14,8 +14,8 @@ module load trimmomatic
 java -jar /sw/apps/bioinfo/trimmomatic/0.36/rackham/trimmomatic-0.36.jar PE -phred33 \
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/raw/sel3_SRR1719266.2.fastq.gz \
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/raw/sel3_SRR1719266.1.fastq.gz \
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719266.trim_2P.fastq.gz \
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719266.trim_2U.fastq.gz \
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719266.trim_1P.fastq.gz \
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719266.trim_1U.fastq.gz \
+/home/stellab/private/genome_analysis/results/2_trimming/sel3_SRR1719266.trim_2P.fastq.gz \
+/home/stellab/private/genome_analysis/results/2_trimming/sel3_SRR1719266.trim_2U.fastq.gz \
+/home/stellab/private/genome_analysis/results/2_trimming/sel3_SRR1719266.trim_1P.fastq.gz \
+/home/stellab/private/genome_analysis/results/2_trimming/sel3_SRR1719266.trim_1U.fastq.gz \
 ILLUMINACLIP:/sw/bioinfo/trimmomatic/0.36/rackham/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
