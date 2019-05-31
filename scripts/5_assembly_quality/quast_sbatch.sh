5a9ebde0bba1bc17e29c1f11fc33381c75b34df5
stellabelin <stella.belin@me.com>
2019-05-31 19:51:07 +0200

    Removed redundant files

--- scripts/5_assembly_quality/quast_sbatch.sh
+++ scripts/5_assembly_quality/quast_sbatch.sh
@@ -11,8 +11,8 @@
 module load bioinfo-tools
 module load quast
 
-quast.py /home/stellab/private/genome_analysis/results/3_genome_assembly/spades/scaffolds.fasta \
-/home/stellab/private/genome_analysis/results/3_genome_assembly/kmer_49/output.scafSeq \
--o /home/stellab/private/genome_analysis/results/5_assembly_quality/quast/comparison \
+quast.py /home/stellab/private/genome_analysis/results/5_assembly_quality/quast/all/spades.fasta \
+/home/stellab/private/genome_analysis/results/5_assembly_quality/quast/all/soapdenovo_27.fasta \
+/home/stellab/private/genome_analysis/results/5_assembly_quality/quast/all/soapdenovo_49.fasta \
+-o /home/stellab/private/genome_analysis/results/5_assembly_quality/quast/all \
 -R /home/stellab/private/genome_analysis/raw_data/unzipped.fasta
-
