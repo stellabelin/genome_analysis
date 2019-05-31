c1b205260690e21515b2802a35939619d012b77c
Stella <stella.belin@me.com>
2019-05-31 17:24:59 +0200

    TopHat cs17

--- scripts/7_differential_expression/tophat_cs17.sh
+++ scripts/7_differential_expression/tophat_cs17.sh
@@ -14,21 +14,21 @@ module load tophat
 bowtie2-build -f /home/stellab/private/genome_analysis/raw_data/unzipped.fasta /home/stellab/private/genome_analysis/results/7_differential_expression/ref
 
 # cs17 forelimb
-tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/cs17_forelimb/ \
-/home/stellab/private/genome_analysis/results/7_differential_expression/ref \
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_1P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_1P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_1P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_2P.fastq.gz
+#tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/cs17_forelimb/ \
+#/home/stellab/private/genome_analysis/results/7_differential_expression/ref \
+#/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_1P.fastq.gz,\
+#/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_1P.fastq.gz,\
+#/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_1P.fastq.gz,\
+#/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_2P.fastq.gz,\
+#/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_2P.fastq.gz,\
+#/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_2P.fastq.gz
 
 # cs17 hindlimb
 tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/cs17_hindlimb/ \
 /home/stellab/private/genome_analysis/results/7_differential_expression/ref \
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_1P.fastq.gz,\
-/home/stellab/private/genome_analysis/results/2_trimming/sel3_SRR1719266.trim_1P.fastq.gz, \
+/home/stellab/private/genome_analysis/results/2_trimming/sel3_SRR1719266.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_2P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_2P.fastq.gz,\
 /home/stellab/private/genome_analysis/results/2_trimming/sel3_SRR1719266.trim_2P.fastq.gz 
