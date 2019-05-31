e66818849032b568db11605ce9ff51ffeb65865c
Stella <stella.belin@me.com>
2019-05-31 19:13:34 +0200

    Modified tophat for all stages

--- scripts/7_differential_expression/tophat_sbatch.sh
+++ scripts/7_differential_expression/tophat_sbatch.sh
@@ -13,73 +13,62 @@ module load tophat
 
 bowtie2-build -f /home/stellab/private/genome_analysis/raw_data/unzipped.fasta /home/stellab/private/genome_analysis/results/7_differential_expression/ref
 
-tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/ \
+# cs15 forelimb
+tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/cs15_forelimb/ \
 /home/stellab/private/genome_analysis/results/7_differential_expression/ref \
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719013.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719014.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719015.trim_1P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719013.trim_2P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719014.trim_2P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719015.trim_2P.fastq.gz
+
+# cs15 hindlimb
+tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/cs15_hindlimb/ \
+/home/stellab/private/genome_analysis/results/7_differential_expression/ref \
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719016.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719017.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719018.trim_1P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719016.trim_2P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719017.trim_2P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719018.trim_2P.fastq.gz
+
+# cs16 forelimb
+tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/cs16_forelimb/ \
+/home/stellab/private/genome_analysis/results/7_differential_expression/ref \
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719204.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719207.trim_1P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719204.trim_2P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_2P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719207.trim_2P.fastq.gz
+
+# cs16 hindlimb
+tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/cs16_hindlimb/ \
+/home/stellab/private/genome_analysis/results/7_differential_expression/ref \
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719212.trim_1P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719214.trim_1P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719242.trim_1P.fastq.gz \
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719212.trim_2P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719214.trim_2P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719242.trim_2P.fastq.gz
+
+# cs17 forelimb
+tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/cs17_forelimb/ \
+/home/stellab/private/genome_analysis/results/7_differential_expression/ref \
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_1P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719212.trim_1P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_1P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719214.trim_1P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_1P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719242.trim_1P.fastq.gz \
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719013.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719014.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719015.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719016.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719017.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719018.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719204.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719207.trim_2P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_2P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719212.trim_2P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_2P.fastq.gz
+
+# cs17 hindlimb
+tophat -o /home/stellab/private/genome_analysis/results/7_differential_expression/cs17_hindlimb/ \
+/home/stellab/private/genome_analysis/results/7_differential_expression/ref \
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_1P.fastq.gz,\
+/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_1P.fastq.gz,\
+/home/stellab/private/genome_analysis/results/2_trimming/sel3_SRR1719266.trim_1P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719214.trim_2P.fastq.gz,\
 /home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_2P.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719242.trim_2P.fastq.gz \
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719013.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719014.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719015.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719016.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719017.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719018.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719204.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719207.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719212.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719214.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719242.trim_1U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719013.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719014.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719015.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719016.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719017.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719018.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719204.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719207.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719212.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719214.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_2U.fastq.gz,\
-/home/stellab/private/genome_analysis/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719242.trim_2U.fastq.gz \
+/home/stellab/private/genome_analysis/results/2_trimming/sel3_SRR1719266.trim_2P.fastq.gz 
